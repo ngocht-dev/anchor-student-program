9a29fbefe3124e93394c463f135903f7cf5d43ad
Admin <admin@Admins-MacBook-Pro.local>
2024-10-05 13:37:49 +0700

    update testing

--- programs/anchor-student-program/src/lib.rs
+++ programs/anchor-student-program/src/lib.rs
@@ -2,15 +2,118 @@ use anchor_lang::prelude::*;
 
 declare_id!("HdBggj1AtyVj5gnM3Ru2A2dVXijFgQZQQX6i5q8xU3JN");
 
+const MAX_NAME_LENGTH: usize = 20;
+const MAX_DESCRIPTION_LENGTH: usize = 50;
+const DISCRIMINATOR: usize = 8;
+
 #[program]
 pub mod anchor_student_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn add_student(ctx: Context<AddStudent>, name: String, description: String) -> Result<()> {
+        // We require that the title is not longer than 20 characters
+        require!(name.len() <= MAX_NAME_LENGTH, StudentError::NameTooLong);
+
+        // We require that the description is not longer than 50 characters
+        require!(
+            description.len() <= MAX_DESCRIPTION_LENGTH,
+            StudentError::DescriptionTooLong
+        );
+
+        msg!("Student Account Created");
+        msg!("Name: {}", name);
+        msg!("Description: {}", description);
+
+        let student = &mut ctx.accounts.student;
+        student.creator = ctx.accounts.initializer.key();
+        student.name = name;
+        student.description = description;
+        Ok(())
+    }
+
+    pub fn update_student(
+        ctx: Context<UpdateStudent>,
+        name: String,
+        description: String,
+    ) -> Result<()> {
+        msg!("Student account space reallocated");
+        msg!("Name: {}", name);
+        msg!("Description: {}", description);
+
+        let student = &mut ctx.accounts.student;
+        student.description = description;
+
+        Ok(())
+    }
+
+    pub fn delete_student(_ctx: Context<DeleteStudent>, name: String) -> Result<()> {
+        msg!("Student for {} deleted", name);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name:String, description:String)]
+pub struct AddStudent<'info> {
+    #[account(
+        init_if_needed,
+        seeds = [name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = DISCRIMINATOR + Student::INIT_SPACE
+    )]
+    pub student: Account<'info, Student>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name:String, description:String)]
+pub struct UpdateStudent<'info> {
+    #[account(
+        mut,
+        seeds = [name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = DISCRIMINATOR + Student::INIT_SPACE,
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub student: Account<'info, Student>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct DeleteStudent<'info> {
+    #[account(
+        mut,
+        seeds=[name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub student: Account<'info, Student>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Student {
+    pub creator: Pubkey, // 32
+    #[max_len(20)]
+    pub name: String, // 4 + len()
+    #[max_len(50)]
+    pub description: String, // 4 + len()
+}
+
+#[error_code]
+enum StudentError {
+    #[msg("Student Name too long")]
+    NameTooLong,
+    #[msg("Student Description too long")]
+    DescriptionTooLong,
+}
--- tests/anchor-student-program.ts
+++ tests/anchor-student-program.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorStudentProgram } from "../target/types/anchor_student_program";
+import { expect } from "chai";
 
 describe("anchor-student-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorStudentProgram as Program<AnchorStudentProgram>;
+  const program = anchor.workspace
+    .AnchorStudentProgram as Program<AnchorStudentProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const student = {
+    name: "Pi Huynh",
+    description: "I am a solana on-chain developer",
+  };
+
+  const [studentPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(student.name), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  it("fetch a student account", async () => {
+    try {
+      const account = await program.account.student.fetch(studentPda);
+      console.log(account);
+    } catch (error) {
+      console.log("fetch a student account failed: ", error);
+    }
   });
+
+  // it("Student is added`", async () => {
+  //   // Add your test here.
+  //   await program.methods.addStudent(student.name, student.description).rpc();
+
+  //   const account = await program.account.student.fetch(studentPda);
+  //   expect(student.name === account.name);
+  //   expect(student.description === account.description);
+  //   expect(account.creator === provider.wallet.publicKey);
+  // });
+
+  // it("Student is updated`", async () => {
+  //   const newDescription =
+  //     "I am a solana on-chain developer and a full-stack developer";
+  //   await program.methods.updateStudent(student.name, newDescription).rpc();
+
+  //   const account = await program.account.student.fetch(studentPda);
+  //   expect(student.name === account.name);
+  //   expect(newDescription === account.description);
+  //   expect(account.creator === provider.wallet.publicKey);
+  // });
+
+  // it("Deletes a student", async () => {
+  //   await program.methods.deleteStudent(student.name).rpc();
+  // });
 });
